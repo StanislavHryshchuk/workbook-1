8c383d0881f6cdbe23e906924bdf5fd86b2a3070
Stanislav Hryschuk <stas.hryshchuk@gmail.com>
2025-04-22 19:57:28 -0700

    Last update

---
+++ IntelliJ/javDataTypes/src/week_3/writingReadingFile/userAddBook.java
@@ -0,0 +1,41 @@
+package week_3.writingReadingFile;
+
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.Arrays;
+import java.util.InputMismatchException;
+import java.util.Scanner;
+
+public class userAddBook {
+    static Scanner scanner = new Scanner(System.in);
+
+    public static void main(String[] args) {
+        writingFile();
+    }
+
+    public static void writingFile(){
+        try (FileWriter fr = new FileWriter("index1.txt")){
+
+            int count = 0;
+            String [] userTitles = new String[3];
+            String [] userAuthors = new String[3];
+            Book[] books = new Book[3];
+            while (count < 3) {
+                System.out.println("Please enter the Title and Author of the Book " + (count + 1));
+                books[count] = new Book(scanner.nextLine(),scanner.nextLine());
+                count++;
+            }
+            for (Book book: books){
+                fr.write(book.getTitle() + "|" + book.getAuthor() + "\n");
+            }
+
+//                books [count] = new Book(scanner.nextLine(),scanner.nextLine());
+//                fr.write(String.valueOf(new Book(scanner.nextLine(),scanner.nextLine())));
+//                fr.write(fr.append(scanner.nextLine()).toString());
+
+
+        }catch (IOException | InputMismatchException e){
+            System.out.println(e.getMessage());
+        }
+    }
+}
